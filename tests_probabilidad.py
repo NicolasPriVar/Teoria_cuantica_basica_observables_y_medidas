d2480a8ba4317318b74de5bd04850d8e947e8251
NicolasPriVar <142689710+NicolasPriVar@users.noreply.github.com>
2023-10-05 09:55:45 -0500

    Update tests_probabilidad.py

--- tests_probabilidad.py
+++ tests_probabilidad.py
@@ -12,6 +12,11 @@ class TestCibreriaComplejos(unittest.TestCase):
         self.assertAlmostEqual(v, 0.12247448713915893+0.5103103630798287j)
         v1=probabilidad.transicion([2+1j, -3j, 4], [3+5j, 6+3j, 1+0j])
         self.assertAlmostEqual(v1, 0.12247448713915893-0.5103103630798287j)
+    def test_probabilidad_transicion(self):
+        v=probabilidad.probabilidadtrans([3+5j, 6+3j, 1+0j], [2+1j, -3j, 4])
+        self.assertAlmostEqual(v, 0.2754166666666666)
+        v1=probabilidad.probabilidadtrans([1+1j, 3-3j, 2+6j], [2+1j, 3-5j, 2+3j])
+        self.assertAlmostEqual(v1, 0.8237179487179488)
     
 if __name__ == '__main__':
     unittest.main()
